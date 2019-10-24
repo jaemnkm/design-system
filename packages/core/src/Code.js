6f155e6b9ec925bd6cd7f3eec48c57567e2617f7
Craig Palermo <craig.palermo@priceline.com>
2019-10-24 10:55:05 -0400

    Add comments to Code

--- packages/core/src/Code.js
+++ packages/core/src/Code.js
@@ -4,19 +4,31 @@ import { fontSize, lineHeight, space } from 'styled-system'
 import { mapProps, applyVariations, getPaletteColor } from './utils'
 
 const Code = mapProps(({ align, ...props }) => ({
+  // Map React props to new prop names that will be passed to styled component
+  // Useful for aliasing deprecated props
   ...props
-}))(styled.code`
-  color: ${getPaletteColor('base')};
-  ${props =>
-    props.bg
-      ? `background-color: ${getPaletteColor(props.bg, 'base')(props)};`
-      : ''}
-  ${applyVariations('Code')}
-  ${fontSize}
-  ${lineHeight}
-  ${space}
-  white-space: pre-wrap;
-`)
+}))(
+  // Create styled component
+  styled.code`
+    // Regular old CSS rules
+    color: ${getPaletteColor('base')};
+    white-space: pre-wrap;
+    
+    // Use prop to get background color from palette
+    ${props =>
+      props.bg
+        ? `background-color: ${getPaletteColor(props.bg, 'base')(props)};`
+        : ''}
+    
+    // For white label
+    ${applyVariations('Code')}
+
+    // Styled system functions
+    ${fontSize}
+    ${lineHeight}
+    ${space}
+`
+)
 
 Code.displayName = 'Code'
 
