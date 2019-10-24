0cf5b9d96cb9f93bb95c829b9044ce756d66429d
Craig Palermo <craig.palermo@priceline.com>
2019-10-24 13:58:58 -0400

    Add plop config for core component

--- plopfile.js
+++ plopfile.js
@@ -1,7 +1,36 @@
 module.exports = function(plop) {
   // create your generators here
-  plop.setGenerator('component', {
-    description: 'React component',
+
+  plop.setGenerator('core', {
+    description: 'Create new core component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'componentName',
+        message: 'Please enter component name (ProperCase)'
+      }
+    ], // array of inquirer prompts
+    actions: [
+      {
+        type: 'add',
+        path: 'packages/core/src/{{componentName}}.js',
+        templateFile: 'plop-templates/core-component/Component.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'packages/core/test/{{componentName}}.js',
+        templateFile: 'plop-templates/core-component/Test.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'packages/core/storybook/{{componentName}}.js',
+        templateFile: 'plop-templates/core-component/Story.js.hbs'
+      }
+    ] // array of actions
+  })
+
+  plop.setGenerator('sub', {
+    description: 'Create new sub-module',
     prompts: [
       {
         type: 'input',
